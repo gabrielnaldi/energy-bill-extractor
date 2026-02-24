dfe6815c963baf890f7ff11f9de2d6c8bb8ed3ef
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-02-24 11:29:08 -0300

    feat: create eletric quantity value object

---
+++ src/domain/value-objects/eletric-quantity.value-object.ts
@@ -0,0 +1,20 @@
+export class EletricQuantity {
+  private _value: number;
+
+  private constructor(value: number) {
+    this._value = value;
+  }
+
+  // Getters
+  get value() {
+    return this._value;
+  }
+
+  public static create(value: number) {
+    if (value < 0) throw new Error('Eletric quantity can not be negative!');
+
+    const eletricQuantity = new EletricQuantity(value);
+
+    return eletricQuantity;
+  }
+}
