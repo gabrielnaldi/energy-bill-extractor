3e77d81f87f19c2d147a3fa3426c8ad7857bce53
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-02-24 11:29:25 -0300

    test: ensure it is possible to create an eletric quantity value object

---
+++ src/__tests__/domain/value-objects/eletric-quantity.value-object.spec.ts
@@ -0,0 +1,12 @@
+import { EletricQuantity } from '@src/domain/value-objects/eletric-quantity.value-object';
+
+describe('Eletric quantity - value object', () => {
+  it('should be able to create an eletric quantity value object', () => {
+    const value = 100;
+
+    const eletricQuantity = EletricQuantity.create(value);
+
+    expect(eletricQuantity).toBeInstanceOf(EletricQuantity);
+    expect(eletricQuantity.value).toBe(100);
+  });
+});
